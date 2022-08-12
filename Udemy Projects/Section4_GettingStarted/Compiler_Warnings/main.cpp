3c50a485545c14a1e6959a00a1ded852496533c4
Blake Bishop <94153853+BalakayBishop@users.noreply.github.com>
2022-08-12 09:44:58 -0700

    main for Compiler_Warnings

---
+++ Udemy Projects/Section4_GettingStarted/Compiler_Warnings/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream> 
+
+int main()
+{
+    int miles_driven;
+    
+    std::cout << miles_driven << std::endl; // uninitilized int value 
+    
+    // or 
+    int favorite_number = 26; // unused variable 
+    
+    
+    
+    return 0; 
+}
