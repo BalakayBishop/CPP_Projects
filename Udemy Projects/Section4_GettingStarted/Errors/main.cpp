a7ec7772fcebe941238b520d499aa55ecdb87cc0
Blake Bishop <94153853+BalakayBishop@users.noreply.github.com>
2022-08-12 09:47:17 -0700

    linker and runtime error mains for Errors

---
+++ Udemy Projects/Section4_GettingStarted/Errors/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream> 
+
+int main()
+{
+    std :: cout << "My template" << std :: endl; 
+    
+    return 0; 
+}
+
+/*
+ * error that occur when the project is executing 
+ * dividing by zero, file not found, out of memeory, etc.
+*/
