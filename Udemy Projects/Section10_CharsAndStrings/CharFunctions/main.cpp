6550d8ddac25865437e89b02a534a793da133475
Blake Bishop <94153853+BalakayBishop@users.noreply.github.com>
2022-08-12 15:09:28 -0700

    Add files via upload

---
+++ Udemy Projects/Section10_CharsAndStrings/CharFunctions/main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <cctype>
+
+using namespace std;
+
+int main()
+{
+    char c {};
+    
+    
+    return 0;
+}
+/*
+ * cctype allows for different methods to be used on character literals 
+ * 
+ * isalpha = a letter
+ * isalnum = a letter or digit
+ * isdigit = a digit
+ * islower = lowercase letter
+ * isprint = is a printable char
+ * ispunct = is punctuation char
+ * isupper = an uppercase letter
+ * isspace = a whitespace
+ * 
+ */
