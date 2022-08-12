009aa797b0f3e83d754da8c76bffdb215e745309
Blake Bishop <94153853+BalakayBishop@users.noreply.github.com>
2022-08-12 09:43:59 -0700

    main for First_Program

---
+++ Udemy Projects/Section4_GettingStarted/First_Program/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int favorite_number; 
+    cout << "Please enter you favorite number between 1 - 100: "; 
+    cin >> favorite_number; 
+    cout << "Wow! " << favorite_number << " is my favorite number too!" << endl;
+    
+    return 0; 
+}
