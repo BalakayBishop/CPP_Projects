8c800b6f7cd30e99db2433ab3c92d944dae59ee3
Blake Bishop <94153853+BalakayBishop@users.noreply.github.com>
2022-09-15 14:19:24 -0700

    Add files via upload

---
+++ Udemy Projects/Section13_OOP/OOP/main.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    
+    
+    return 0;
+}
+/*
+ * Procedural Programming
+ *      the fucos is the function, creating functions
+ *      data is declared separately
+ *      data is passed as arguments into functions
+ *      
+ * Object-Oriented Programming
+ *      all about modeling in terms of classes and objects 
+ * 
+ * Encapsulation - an abstract data type 
+ * 
+ * Information hiding - hiding implementation-specific logic 
+ *      
+ * All about reusability 
+ *      faster development 
+ *      higher quality 
+ * 
+ * Inheritence 
+ *      can create new classes in term of existing classes
+ *      polymorphic classes 
+ * 
+ * Classes 
+ *      blueprint for which objects are created 
+ *      user-defined data-type 
+ *      attribtues (data)
+ *      functions (methods)
+ *      can hide data and methods via private and public keywords
+ *      Example: Account, employee, vector, string
+ * Objects
+ *      created from classes
+ *      a specific instance of a class 
+ *      can have as many objects as needed
+ *      each obj has its own idenity
+ *      Example : Account frank_account; an object named frank_account of class Account
+ * 
+ * 
+ */
