81f4b602520cb4ef13da8ca06de631f1361b3b9a
Blake Bishop <94153853+BalakayBishop@users.noreply.github.com>
2022-09-15 14:21:46 -0700

    Add files via upload

---
+++ Udemy Projects/Section13_OOP/Structs/main.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Person {
+    string name; 
+};
+
+struct Persons {
+    string name;
+};
+
+int main()
+{
+    
+    
+    return 0;
+}
+/* 
+ * Struct or Structure in addition to a class 
+ * 
+ * struct comes from C
+ * 
+ * essentially the same as a class but members are public by default
+ * 
+ * Struct 
+ *      use struct for passive objects with public access
+ *      dont declare methods in struct
+ * 
+ * Class 
+ *      use for active with private access
+ *      implement getters and setters
+ *      member methods as needed
+ * 
+ * 
+ * 
+ */
