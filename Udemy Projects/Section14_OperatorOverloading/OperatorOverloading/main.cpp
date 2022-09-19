0621810eea9beae4a86251ff9985abebe6de0fca
Blake Bishop <94153853+BalakayBishop@users.noreply.github.com>
2022-09-19 16:34:13 -0700

    Add files via upload

---
+++ Udemy Projects/Section14_OperatorOverloading/OperatorOverloading/main.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    
+    
+    return 0;
+}
+/* 
+ * Operator Overloading
+ *      overload most operators to work with user defined classes
+ *      assignment operator - default assignment
+ *      overload as member and global function 
+ *      we can also overload << and >> 
+ * 
+ * Allows user defined type to behave similar to built-in types 
+ * Can make code more readable
+ * All other overlaoding other than = must be done explicitly
+ * Function example: result = multiply(add(a,b), divide(c,d));
+ * Method example: result = (a.add(b)).multiply(c.divide(d));
+ * 
+ * Cannot be overloaded
+ *      ::, :?, .*, ., and sizeof
+ * 
+ * precendence and associativity cannot be changed 
+ * arity cannot be changed
+ * cannot overload operators for primitive types 
+ * cannot creat new coperators
+ * 
+ * 
+ * 
+ * 
+ */
