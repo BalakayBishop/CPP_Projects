f717f25b66fb9e18f81f9bea6366d66fba5a44e8
Blake Bishop <94153853+BalakayBishop@users.noreply.github.com>
2022-09-28 14:13:07 -0700

    Add files via upload

---
+++ Udemy Projects/Section16_Polymorphism/FinalSpecifier/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    
+    
+    return 0;
+}
+/*
+ * C++11 provides the final specified
+ * when used at the class level
+ *      prevents a class from being derived from 
+ * 
+ * when used at the method level
+ *      prevents virtual methods from being overridden in derived classes
+ * 
+ * 
+ */
