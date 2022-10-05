383ba16900df90b240287ed5400bb01866199004
Blake Bishop <94153853+BalakayBishop@users.noreply.github.com>
2022-10-05 14:44:18 -0700

    Add files via upload

---
+++ Udemy Projects/Section19_IOStreams/File IO and Streams/main.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    
+    
+    return 0;
+}
+/*
+ * Files, Streams, and IO
+ *      stream abstraction to work with IO
+ *      abstraction to use with interface 
+ *      
+ *      input device -> input stream -> program
+ *      program -> output stream -> output device
+ * 
+ * Header File
+ *      iostream - formatted input and output   
+ *      fstream - formatted IO to a from files
+ *      iomanip - manipulators to format stream IO
+ * 
+ *      ios -   
+ *      ifstream - high-level input
+ *      ofstream - high-level output
+ *      fstream - derived from i and o fstream
+ *      stringstream - memory based string
+ * 
+ *      cin - input from stream
+ *      cout - output to console, buffered
+ *      cerr - error, unbuffered
+ *      clog - log stream, unbuffered 
+ * 
+ * Stream Manipulators
+ *      memeber or manipulator version
+ *      
+ * 
+ */
